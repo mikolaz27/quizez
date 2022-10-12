e8ba695cbafaf845b370f8f99ad7e3e041ece17f
Михайло Лазорик <mikolaz2727@gmail.com>
2022-10-12 21:22:51 +0300

    Added docker compose + fix of ci/cd

--- docker_example/hello_world_from_docker.py
+++ docker_example/hello_world_from_docker.py
@@ -12,14 +12,5 @@ while True:
             "col2": [3, 4],
         }
     )
-    # print(datetime.datetime.now())
     print(df)
     sleep(1)
-
-
-# docker run --rm -d -p 8010:8008 --name quizez_cont quizez
-# docker build -t quizez .
-
-# docker run --rm -d -p 8010:8008 -v D:\Hillel\quizez\quizez\src:/quizez --name quizez_cont quizez
-
-# docker run --rm -d -p 8010:8008 -v D:\Hillel\quizez\quizez:/quizez --name quizez_cont quizez ./commands/start_server.sh
--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -9,19 +9,31 @@ print(CURRENT_ENV)
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = []
 
-DATABASES = {
-    "default_sql": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT"),
-    },
-}
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "github_actions",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        },
+    }
+else:
+    DATABASES = {
+        "default_sql": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
+        },
+    }
 
 # INSTALLED_APPS += ""
