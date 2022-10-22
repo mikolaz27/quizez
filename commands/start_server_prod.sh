d2a7cc7482aeb9938210e48787df6d3aa8a9ad92
Михайло Лазорик <mikolaz2727@gmail.com>
2022-10-22 14:00:53 +0300

    Added wsgi server

--- commands/start_server_prod.sh
+++ commands/start_server_prod.sh
@@ -3,4 +3,4 @@
 python src/manage.py migrate
 python src/manage.py collectstatic --noinput
 
-python src/manage.py runserver 0:${WSGI_PORT}
+gunicorn -w ${WSGI_WORKERS} -b 0:${WSGI_PORT} --chdir ./src config.wsgi:application --log-level=${WSGI_LOG_LEVEL}
