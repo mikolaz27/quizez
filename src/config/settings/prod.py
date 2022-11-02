91784a50e73e86cd948b38d036653a474e54827c
Михайло Лазорик <mikolaz2727@gmail.com>
2022-11-02 21:25:30 +0200

    Prepare to prod

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -7,7 +7,7 @@ print(CURRENT_ENV)
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "ec2-34-229-88-177.compute-1.amazonaws.com"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
@@ -16,11 +16,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DATABASES = {
-    "default_sql": {
+    "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     },
-    "default": {
+    "default_postgres": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
