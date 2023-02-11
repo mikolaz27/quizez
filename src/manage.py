977230a9d6c87b6228df9f2b3ae7237d7b7dceda
Mykhailo Lazoryk <mikolaz2727@gmail.com>
2023-02-11 10:19:47 +0200

    Test push

--- src/manage.py
+++ src/manage.py
@@ -9,6 +9,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     if sys.argv[1] == "test":
+
         print("NOTE: Running black formation:")
         print(os.popen(f"black --config {Path(__file__).resolve().parent.parent}/.black.toml .").read())
         print(os.popen("isort .").read())
